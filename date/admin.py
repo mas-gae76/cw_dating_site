15ba2431ebe99119e1af088ce9d407e85bffcce2
mas-gae76 <alex2904.goryachev@yandex.com>
2022-01-18 18:00:05 +0300

    Added models to admin site

--- date/admin.py
+++ date/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'birthday', 'avatar', 'gender', )
+    list_display_links = ('email', )
+    search_fields = ('first_name', 'last_name', 'email', )
+
+
+@admin.register(UserRating)
+class UserRatingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'rating')
+    search_fields = ('user', )
+
+
+@admin.register(UserSettings)
+class UserRatingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'setting', 'value')
+    search_fields = ('user', 'setting')
+
+
+admin.site.site_header = 'Сайт знакомств GetPair'
