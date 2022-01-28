a6899d37603cd36ce56d7871cfe3729b4412b4e4
mas-gae76 <alex2904.goryachev@yandex.com>
2022-01-29 01:31:02 +0300

    added current year view

--- date/views.py
+++ date/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
 from .models import *
+from datetime import datetime
 
 
 class UserView(ListView):
@@ -7,5 +8,11 @@ class UserView(ListView):
     paginate_by = 50
     queryset = User.objects.exclude(is_staff=True)
     template_name = 'index.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_year'] = datetime.today().year
+        return context
+
 
 
