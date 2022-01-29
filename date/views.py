3f8bbe468ecab8314f46a09aad4e6936dc8f0fa5
mas-gae76 <alex2904.goryachev@yandex.com>
2022-01-29 18:01:34 +0300

    added reqister view and template

--- date/views.py
+++ date/views.py
@@ -1,18 +1,27 @@
 from django.views.generic import ListView
 from .models import *
 from datetime import datetime
+from django.contrib.auth.views import LoginView
+from django.views.generic import CreateView
+from .forms import RegisterForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 
 class UserView(ListView):
     model = User
     paginate_by = 50
-    queryset = User.objects.exclude(is_staff=True)
     template_name = 'index.html'
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['current_year'] = datetime.today().year
-        return context
+    queryset = User.objects.exclude(is_staff=True)
+    extra_context = {'current_year': datetime.today().year}
 
 
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = 'signup.html'
+    extra_context = {'current_year': datetime.today().year}
 
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('index')
