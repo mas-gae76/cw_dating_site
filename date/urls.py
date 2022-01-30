aaf6d6bb059f43812b6d1125934cce1002ce85cf
mas-gae76 <alex2904.goryachev@yandex.com>
2022-01-30 17:03:28 +0300

    added login/logout form

--- date/urls.py
+++ date/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = [
     url(r'^signup$', RegisterView.as_view(), name='signup'),
+    url(r'^login$', UserLoginView.as_view(), name='login'),
+    url(r'^logout$', UserLogoutView.as_view(), name='logout'),
     url(r'^$', UserView.as_view(), name='index'),
 ]
