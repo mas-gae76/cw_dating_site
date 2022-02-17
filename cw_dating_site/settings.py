b6715144b79e5d44503766dbdfb1c08d665be1a4
mas-gae76 <alex2904.goryachev@yandex.com>
2022-02-17 21:13:06 +0300

    added Redis caching

--- cw_dating_site/settings.py
+++ cw_dating_site/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -102,6 +103,18 @@ DATABASES = {
         'PORT': 5432
     }
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
+        }
+    }
+}
+
+CACHE_TTL = 60 * 5
 
 
 # Password validation
